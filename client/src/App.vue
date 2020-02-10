938ae0618f98033b11da93ee3be61d0e6e66a846
King Cobra <kcobra557@gmail.com>
2020-02-10 04:31:16 +0000

    added new header in the css component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="container">hello</div>
+  <div class="container">
+    <Header />
+  </div>
 </template>
 
 <script>
-
-
+import Header from './components/Header';
 export default {
   name: 'App',
   components: {
-    
+    Header
   }
 }
 </script>
